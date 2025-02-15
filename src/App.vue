49db58b806b8e42412b23d843f8eb7d75adae1b0
hihajdus <sebastian.hajdus@gmail.com>
2025-02-15 17:55:31 +0100

    component and accessibility highlighting added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ToggleView />
+  <HelloWorld msg/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ToggleView from './components/toggleView.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, ToggleView
   }
 }
 </script>
---
+++ src/components/toggleView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <button @click="toggleAccessibility">Toggle Accessibility</button>
+    <article>
+      <h1 :aria-hidden="accessibilityEnabled ? 'false' : 'true'" :class="{'highlight': accessibilityEnabled}">Sample Article</h1>
+      <p :aria-live="accessibilityEnabled ? 'polite' : 'off'" :class="{'highlight': accessibilityEnabled}">
+        This is a paragraph demonstrating accessibility attributes.
+      </p>
+      <img src="../assets/logo.png" alt="Placeholder Image"
+           :aria-describedby="accessibilityEnabled ? 'imageDesc' : ''"
+           :class="{'highlight': accessibilityEnabled}"/>
+      <p v-if="accessibilityEnabled" id="imageDesc" class="highlight">This is a description of the image.</p>
+      <ul>
+        <li v-for="(item, index) in items" :key="index" :aria-label="accessibilityEnabled ? `Item ${index + 1}` : ''"
+            :class="{'highlight': accessibilityEnabled}">
+          List Item {{ index + 1 }}
+        </li>
+      </ul>
+      <button :aria-pressed="accessibilityEnabled ? 'true' : 'false'" :class="{'highlight': accessibilityEnabled}">
+        Click Me
+      </button>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      accessibilityEnabled: false,
+      items: ["Item 1", "Item 2", "Item 3"]
+    };
+  },
+  methods: {
+    toggleAccessibility() {
+      this.accessibilityEnabled = !this.accessibilityEnabled;
+    }
+  }
+};
+</script>
+
+<style>
+.highlight {
+  color: blue;
+  font-weight: bold;
+}
+</style>
